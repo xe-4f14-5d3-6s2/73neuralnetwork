aed8640f68d311b416c3f604e2f3b32db199eaa2
Just Tantalum <xe.4f14.5d3.6s2@proton.me>
2025-12-06 11:16:42 +0000

    Borrón y cuenta nueva

--- tests/loading.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from main import Connection, Model, Neuron
-
-model = Model.load("/home/tantalumdev/Documents/Proyectos/RedNeuronal/56cf25bc-2f47-427d-9895-1af339846de0.73nn")
-
-model.predict([35])
--- tests/saving.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from main import Connection, Model, Neuron
-
-model = Model.new(1, [])
-model.save()
