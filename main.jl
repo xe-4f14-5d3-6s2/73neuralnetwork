d852b1b691745cd61809034061f8e316d718e27b
Just Tantalum <xe.4f14.5d3.6s2@proton.me>
2025-12-06 15:42:48 +0000

    feat: ✨ Add mutable Neuron struct with Float64 fields feat: ✨ Add Connection struct with Float64 weight feat: ✨ Implement Model with nested neuron arrays and activation type

--- main.jl
+++ main.jl
@@ -1,20 +1,49 @@
 using Distributions
 
-struct Neuron
+############# Neuron
+mutable struct Neuron
 	x::Vector
-	a::Float32
-	z::Float32
-	bias::Float32
+	a::Float64
+	z::Float64
+	bias::Float64
 end
 Neuron() = Neuron([], 0.0, 0.0, rand(Uniform(-1.0, 1.0)))
+############# End Neuron
 
-struct Connection
+################# Connection
+mutable struct Connection
 	neuron::Neuron
-	weight::Float32
+	weight::Float64
 end
 Connection(neuron::Neuron) = Connection(neuron, rand(Uniform(-1.0, 1.0)))
+################# End Connection
 
-struct Model
-	network::Matrix{Neuron}
+############ Model
+mutable struct Model
+	network::Array{Array{Neuron, 1},1}
 	activaction::String
 end
+
+function Model_generate_layer(n::Int64)
+	return fill(Neuron(), n)
+end
+
+function Model_generate_network(layers::Vector{Int64})
+	return [Model_generate_layer(n) for n in layers]
+end
+
+function Model(layers::Vector{Int64}, activaction::String)
+	m = Model(Model_generate_network(layers), activaction)
+
+	for (L, layer) in enumerate(m.network)
+		if L != 1
+			for neuron in layer
+				neuron.x = [Connection(n) for n in m.network[L-1]]
+			end
+		end
+	end
+
+	return m
+
+end
+############ End Model
